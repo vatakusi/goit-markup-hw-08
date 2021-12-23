ff194b3b19d54a0815893d9e227141e067ad2471
Alexandr Morozov // chillipepper <newsskyx@gmail.com>
2021-12-23 07:25:12 +0200

    3

---
+++ js/menu.js
@@ -0,0 +1,35 @@
+/* (() => {
+   const menuBtnRef = document.querySelector("[data-menu-button]");
+
+   menuBtnRef.assEventListener("click", () => {
+
+      menuBtnRef.classList.toggle("is-open");
+
+   });
+}) (); */
+
+/* (() => {
+    const menuBtnRef = document.querySelector("[data-menu-button]");
+    const mobileMenuRef = document.querySelector("[data-menu]");
+    menuBtnRef.addEventListener("click", () => {
+        const expanded =
+            menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+        menuBtnRef.classList.toggle("is-open");
+        menuBtnRef.setAttribute("aria-expanded", !expanded);
+        document.body.classList.toggle("modal-open");
+        mobileMenuRef.classList.toggle("is-open");
+    });
+})(); */
+
+(() => {
+    const menuBtnRef = document.querySelector("[data-menu-button]");
+    const mobileMenuRef = document.querySelector("[data-menu]");
+    menuBtnRef.addEventListener("click", () => {
+        const expanded =
+            menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+        menuBtnRef.classList.toggle("is-open");
+        menuBtnRef.setAttribute("aria-expanded", !expanded);
+        document.body.classList.toggle("modal-open");
+        mobileMenuRef.classList.toggle("is-open");
+    });
+})();
